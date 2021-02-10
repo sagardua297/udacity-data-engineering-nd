128fb7eaf2efbda5dde43e3f63c9cf94eaf39203
Saga Dua <14318276+sagardua297@users.noreply.github.com>
2021-02-10 17:26:33 +0530

    Update __init__.py

--- Capstone Project/airflow/plugins/operators/__init__.py
+++ Capstone Project/airflow/plugins/operators/__init__.py
@@ -1,5 +1,6 @@
 from operators.airflow_data_quality import DataQualityOperator
 from operators.airflow_data_analytics import DataAnalyticsOperator
+
 __all__ = [
     'DataQualityOperator',
     'DataAnalyticsOperator'
