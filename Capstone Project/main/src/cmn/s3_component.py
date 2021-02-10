d7ca2222e3d5b6eee0084d4fa4ddf96805414ef9
Saga Dua <14318276+sagardua297@users.noreply.github.com>
2021-02-10 17:50:30 +0530

    Update s3_component.py

--- Capstone Project/main/src/cmn/s3_component.py
+++ Capstone Project/main/src/cmn/s3_component.py
@@ -1,9 +1,6 @@
 import boto3
 import configparser
-import logging
 from pathlib import Path
-
-logger = logging.getLogger(__name__)
 
 config = configparser.ConfigParser()
 config.read_file(open(f"{Path(__file__).parents[0]}/s3_config.cfg"))
@@ -39,15 +36,12 @@ class DataPipelineS3Module:
         if target_bucket is None:
             target_bucket = self._processing_store
 
-        logging.debug(f"Inside s3_move_data : Source bucket set is : {source_bucket}\n Target bucket set is : {target_bucket}")
-
         # Cleanup Target bucket
         self.clean_bucket(target_bucket)
 
         # Move files from Source to Target bucket
         for key in self.get_files(source_bucket):
             if key in config.get('FILES','NAME').split(","):
-                logging.debug(f"Copying file {key} from {source_bucket} to {target_bucket}")
                 self._s3.meta.client.copy({'Bucket': source_bucket, 'Key': key}, target_bucket, key)
 
     def get_files(self, bucket_name):
@@ -56,7 +50,6 @@ class DataPipelineS3Module:
         :param bucket_name: bucket to search
         :return: keys or files present in the bucket
         """
-        logging.debug(f"Inspecting bucket : {bucket_name} for files present")
         return [bucket_object.key for bucket_object in self._s3.Bucket(bucket_name).objects.all()]
 
     def clean_bucket(self, bucket_name):
@@ -64,5 +57,4 @@ class DataPipelineS3Module:
         Clean the bucket and delete all files.
         :param bucket_name: bucket name, bucket to clean
         """
-        logging.debug(f"Cleaning bucket : {bucket_name}")
         self._s3.Bucket(bucket_name).objects.all().delete()
