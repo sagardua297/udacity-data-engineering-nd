dedd5115b16c33c7af107b02601a8ba2717a2c09
Saga Dua <14318276+sagardua297@users.noreply.github.com>
2021-02-10 17:31:23 +0530

    Update warehouse_component.py

--- Capstone Project/main/src/warehouse/warehouse_component.py
+++ Capstone Project/main/src/warehouse/warehouse_component.py
@@ -1,12 +1,9 @@
 import configparser
-import logging
 import psycopg2
 from .warehouse_staging_queries import create_staging_schema, drop_staging_tables, create_staging_tables, copy_staging_tables
 from .warehouse_sql_queries import create_warehouse_schema, drop_warehouse_tables, create_warehouse_tables
 from .warehouse_data_process import data_process_queries
 from pathlib import Path
-
-logger = logging.getLogger(__name__)
 
 config = configparser.ConfigParser()
 config.read_file(open(f"{Path(__file__).parents[0]}/warehouse_config.cfg"))
@@ -30,37 +27,30 @@ class DataPipelineWarehouse:
         """
         Create Staging schema and Staging schema tables.
         """
-        logging.debug("Creating schema for staging.")
         self.execute_query([create_staging_schema])
 
-        logging.debug("Dropping Staging tables.")
         self.execute_query(drop_staging_tables)
 
-        logging.debug("Creating Staging tables.")
         self.execute_query(create_staging_tables)
 
     def load_staging_tables(self):
         """
         Populate Staging schema tables.
         """
-        logging.debug("Populating staging tables")
         self.execute_query(copy_staging_tables)
 
     def setup_warehouse_tables(self):
         """
         Create Warehouse schema and Warehouse schema tables.
         """
-        logging.debug("Creating scheam for warehouse.")
         self.execute_query([create_warehouse_schema])
 
-        logging.debug("Creating Warehouse tables.")
         self.execute_query(create_warehouse_tables)
 
     def perform_data_process(self):
         """
         Populate Warehouse schema tables.
         """
-        logging.debug("Performing Upsert.")
         self.execute_query(data_process_queries)
 
     def execute_query(self, query_list):
@@ -70,6 +60,5 @@ class DataPipelineWarehouse:
         """
         for query in query_list:
             print(query)
-            logging.debug(f"Executing Query: {query}")
             self._cur.execute(query)
             self._conn.commit()
