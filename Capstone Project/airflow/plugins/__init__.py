24a00f58a03b0a0bd7631458cb0589bc87747090
Saga Dua <14318276+sagardua297@users.noreply.github.com>
2021-02-10 17:01:41 +0530

    Update __init__.py

--- Capstone Project/airflow/plugins/__init__.py
+++ Capstone Project/airflow/plugins/__init__.py
@@ -1,1 +1,12 @@
+from airflow.plugins_manager import AirflowPlugin
 
+import operators
+import helpers
+
+# Defining the plugin class
+class DataPipelinePlugin(AirflowPlugin):
+    name = "datapipeline_plugin"
+    operators = [
+        operators.DataQualityOperator,
+        helpers.DataAnalyticsOperator
+    ]
